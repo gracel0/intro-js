7d56841233aa6bd52e68bdcb23284e063f77a245
Grace Liang <nieww@yahoo.com>
2019-08-14 19:55:43 -0700

    went over examples on syllabus

---
+++ js/main.js
@@ -0,0 +1,25 @@
+var firstName = 'Grace';
+var age = 10,000,000,000;
+
+// const firstName = 'Grace';
+// firstName = 'G';
+
+
+// old way
+console.log('My name is' + firstName + 'and my age is' + age);
+// better way
+console.log(`My name is ${firstName} and my age is ${age}`);
+
+alert(`Hello, I am ${firstName}`);
+console.log(firstName);
+
+const birthday = '08/14/2019';
+let fixDate = new Date(birthday);
+console.log(fixdate);
+
+let hello = 'hello there';
+alert(hello);
+console.log(typeof(hello));
+
+const firstName = 'Grace';
+console.log(firstName.length);
